2a71ffb4fcdec16cccd30957964774c84369e6ba
Thomas Unterreitmeier <t.unterreitmeier@icloud.com>
2020-05-28 20:06:02 +0200

    initial commit

---
+++ client/client.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+int main()
+{
+    const int port = 3020;
+    const char serverIp[] = "192.168.178.79";
+    unsigned int chunkSize = 4096;
+
+    // create tcp socket
+    int tcpSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (tcpSocket == -1)
+    {
+        std::cerr << "Socket creation failed" << std::endl;
+        return -1;
+    }
+
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(port);
+    inet_pton(AF_INET, serverIp, &hint.sin_addr); // binary ip address from string
+
+    // connect to server
+    if (connect(tcpSocket, (sockaddr *)&hint, sizeof(hint)) != 0)
+    {
+        std::cerr << "Error connecting to server" << std::endl;
+        std::cerr << errno << std::endl;
+        return -2;
+    }
+    std::cout << "Connected to Server on port " << port << std::endl;
+
+    char buffer[chunkSize];
+    system("clear");
+    while (true)
+    {
+        // send data
+        memset(buffer, 0, chunkSize);
+        std::cout << "> ";
+        std::cin >> buffer;
+        if (std::strcmp(buffer, "quit") == 0)
+        {
+            std::cout << "Bye" << std::endl;
+            break;
+        }
+        ssize_t sendStatus = send(tcpSocket, buffer, chunkSize, 0);
+        if (sendStatus == -1)
+        {
+            std::cout << "Message was not sent " << sendStatus << errno << std::endl;
+            continue;
+        }
+
+        // receive response
+        memset(buffer, 0, chunkSize);
+        ssize_t bytesReceived = recv(tcpSocket, buffer, chunkSize, 0);
+        std::cout << "Received data: " << buffer << std::endl;
+        if (bytesReceived == -1)
+        {
+            std::cerr << "Error receiving server data: " << errno << std::endl;
+            break;
+        }
+        if (bytesReceived == 0)
+        { //gracefully closed
+            std::cout << "Server shut down" << std::endl;
+            break;
+        }
+    }
+    shutdown(tcpSocket, SHUT_RDWR);
+}
---
+++ server/server.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+// Idea: define type for socket connections instead of int
+
+int main()
+{
+    const int port = 3020;
+    const char ip[] = "0.0.0.0";
+    unsigned int chunkSize = 4096;
+
+    // create socket
+    int tcpSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (tcpSocket == -1)
+    {
+        std::cerr << "Socket creation failed" << std::endl;
+        return 1;
+    }
+
+    // bind listener to port
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    // hint.sin_addr.s_addr = INADDR_ANY;
+    hint.sin_port = htons(port);            // unsigned short big endian
+    inet_pton(AF_INET, ip, &hint.sin_addr); // binary ip address from string
+
+    if (bind(tcpSocket, (sockaddr *)&hint, sizeof(hint)) != 0)
+    {
+        std::cerr << "Error binding:" << errno << std::endl;
+        return -1;
+    }
+    if (listen(tcpSocket, SOMAXCONN) != 0)
+    {
+        std::cerr << "Error listening:" << errno << std::endl;
+        return -2;
+    }
+
+    system("clear");
+    std::cout << "Listening on port " << port << std::endl;
+
+    // open client socket
+    sockaddr_in client;
+    socklen_t clientSize = sizeof(client);
+
+    char host[NI_MAXHOST];
+    char service[NI_MAXSERV];
+    memset(host, 0, NI_MAXHOST);
+    memset(service, 0, NI_MAXSERV);
+
+    int clientSocket = accept(tcpSocket, (sockaddr *)&client, &clientSize);
+
+    // handle client data
+    int nameInfo = getnameinfo((sockaddr *)&client, clientSize, host, NI_MAXHOST, service, NI_MAXSERV, 0); // get client data and write to mem
+    if (nameInfo != 0)
+    {
+        inet_ntop(AF_INET, &hint.sin_addr, host, clientSize);
+    }
+    std::cout << "Connection from " << host << " on port " << service << std::endl;
+
+    char buffer[chunkSize];
+    while (true)
+    {
+        memset(buffer, 0, chunkSize);
+        ssize_t bytesReceived = recv(clientSocket, buffer, chunkSize, 0);
+        if (bytesReceived == -1)
+        {
+            std::cerr << "Error receiving client data: " << errno << std::endl;
+            break;
+        }
+        if (bytesReceived == 0)
+        {
+            std::cout << "Client disconnected" << std::endl;
+            break;
+        }
+        std::cout << "Received data: " << buffer << std::endl;
+        strlcpy(buffer, "thanks", chunkSize);
+        send(clientSocket, buffer, chunkSize, 0);
+    }
+
+    // close sockets
+    shutdown(clientSocket, SHUT_RDWR);
+
+    shutdown(tcpSocket, SHUT_RDWR);
+}
